844b97fe5a39a0f408a1074724d6b152ef682f21
Zaw Win Tin <zawwintin21@gmail.com>
2021-11-09 21:44:06 +0630

    [feature/post_list] Update nav-bar with css & js

--- Laravel/public/js/common/nav.js
+++ Laravel/public/js/common/nav.js
@@ -1,8 +1,15 @@
-function openNavProfileDropdown() {
-    if($(".profile-dropdown-content").css("display") == "none") {
-        $(".profile-dropdown-content").css("display","block");
+function toggleNavProfileDropdown() {
+    if ($(".profile-dropdown-content").css("display") == "none") {
+        $(".profile-dropdown-content").css("display", "block");
+    } else {
+        $(".profile-dropdown-content").css("display", "none");
     }
-    else {
-        $(".profile-dropdown-content").css("display","none");
-    }    
 }
+
+//Close NavProfileDropdown when click outside of the element
+$(document).on("click", function (event) {
+    var $trigger = $(".nav-dropdown");
+    if ($trigger != event.target && !$trigger.has(event.target).length) {
+        $(".profile-dropdown-content").css("display", "none");
+    }
+});
