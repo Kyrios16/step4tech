ba2c9ea68746715ddc6ad563c6b94b71fd79b877
Zaw Win Tin <zawwintin21@gmail.com>
2021-11-09 17:32:42 +0630

    [feature/post_list] Add navigation before login

---
+++ Laravel/resources/views/layouts/nav.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
+<link rel="stylesheet" href="{{ asset('css/common/nav.css') }}">
+
+<nav id="nav-bar">
+    <div class="nav-wrapper">
+        <div class="nav-list">
+            <h1><a href="#"><img src="{{ asset('img/img_logo.png') }}" alt="Step4Tech"></a></h1>
+            <div class="search-container">
+                <input class="search float-left" type="text" placeholder="Search . . ." name="search"><i class="fa fa-search search-ico"></i>
+            </div> 
+            <!-- Before Login -->            
+            <a href="#" class="nav-btn btn-outline-secondary">Login</a>
+            <a href="#" class="nav-btn btn-primary">Create Account</a>
+
+            <!-- After Login -->
+        </div>             
+    </div>
+</nav>
