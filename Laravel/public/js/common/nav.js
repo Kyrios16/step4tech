5c75a45f5733301c8aa5d123a19b62c3a6700501
Zaw Win Tin <zawwintin21@gmail.com>
2021-11-09 19:08:15 +0630

    [feature/post_list] Add navigation after login

---
+++ Laravel/public/js/common/nav.js
@@ -0,0 +1,8 @@
+function openNavProfileDropdown() {
+    if($(".profile-dropdown-content").css("display") == "none") {
+        $(".profile-dropdown-content").css("display","block");
+    }
+    else {
+        $(".profile-dropdown-content").css("display","none");
+    }    
+}
--- Laravel/resources/views/layouts/nav.blade.php
+++ Laravel/resources/views/layouts/nav.blade.php
@@ -6,13 +6,28 @@
         <div class="nav-list">
             <h1><a href="#"><img src="{{ asset('img/img_logo.png') }}" alt="Step4Tech"></a></h1>
             <div class="search-container">
-                <input class="search float-left" type="text" placeholder="Search . . ." name="search"><i class="fa fa-search search-ico"></i>
+                <input class="search" type="text" placeholder="Search . . ." name="search"><i class="fa fa-search search-ico"></i>
             </div> 
-            <!-- Before Login -->            
-            <a href="#" class="nav-btn btn-outline-secondary">Login</a>
-            <a href="#" class="nav-btn btn-primary">Create Account</a>
+            
+            <!-- Before Login -->  
+            <!-- <div class="nav-before-login">
+                <a href="#" class="nav-btn btn-outline-secondary">Login</a>
+                <a href="#" class="nav-btn btn-primary">Create Account</a>
+            </div>           -->
+            
 
             <!-- After Login -->
+            <div class="nav-dropdown">
+                <img src="" class="profile-ico" alt="Profile">
+                <button class="nav-dropdown-btn" onclick="openNavProfileDropdown()"><i class="fas fa-caret-down"></i></button>                
+                <div class="profile-dropdown-content">
+                    <a href="#">View Profile</a>
+                    <a href="#">Log out</a>
+                </div>
+            </div>
+
         </div>             
     </div>
 </nav>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="{{ asset('js/common/nav.js') }}"></script>
