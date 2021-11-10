18f83a7f42f038c56c825805e8148920b16abb14
may <maythinkhaingminoz@gmail.com>
2021-11-10 11:03:38 +0630

    [feature/sidebar_UI]Edit Navigation and Sidebar UI

--- Laravel/resources/views/layouts/app.blade.php
+++ Laravel/resources/views/layouts/app.blade.php
@@ -15,15 +15,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0/css/v4-shims.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ asset('css/common/reset.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.0/css/fontawesome.min.css">
-    <link href="{{ asset('css/common/sidebar.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common/sidebar.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common/nav.css') }}">
     @yield('style')
 
+    <!-- Script -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.0/js/all.min.js" integrity="sha512-KAubXJ25Ga0L3yytUtDzBpDbP2usQw1dtfmph2QkpIGxFYkdLaXg90k5KVagC7WDZdxwdVzfps9XUrNPnrA++A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{ asset('js/common/sidebar.js') }}"></script>
     <script src="{{ asset('js/common/nav.js') }}"></script>
     @yield('script')
@@ -31,10 +33,10 @@
 </head>
 
 <body>
+    @include('common.nav')
     <div class="main-container">
-        @include('common.nav')
-        <div>
-            @include('common.sidebar') 
+        <div class="clearfix">
+            @include('common.sidebar')
             @yield('content')
         </div>
     </div>
