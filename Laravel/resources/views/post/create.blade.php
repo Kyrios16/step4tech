229ed9d82cd47f0c2b93b371e82c10822a1322a0
may <maythinkhaingminoz@gmail.com>
2021-11-11 14:34:49 +0630

    post-create file create and sidebar like icon change

--- Laravel/resources/views/post/create.blade.php
+++ Laravel/resources/views/post/create.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.app')
 
 @section('style')
-{{-- <link rel="stylesheet" href="{{ asset('css/post/index.css') }}"> --}}
+<link rel="stylesheet" href="{{ asset('css/post/post-create.css') }}">
 @endsection
 
 @section('script')
-{{-- <script src="{{ asset('js/post/index.js') }}"></script> --}}
+{{-- <script src="{{ asset('js/post/post-create.js') }}"></script> --}}
 @endsection
 
 @section('content')
-<h1>Post Create</h1>
+<div class="postCreate-container">
+    <div class="postCreate">
+        <div class="user-info">
+            <img src="{{ asset('img/img_profile_skeleton.png') }}" class="user-profile" alt="Profile">
+            <a class="user-name">User Name</a>
+        </div>
+        <form class="create-form">
+            <input type="text" id="title" name="title" placeholder="Post Title" class="title">
+        </form>
+    </div>
+</div>
 @endsection
