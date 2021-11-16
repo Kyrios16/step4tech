afd3e6add287a53cf282297004fbe5e84fa98c06
maythinzar98 <maythinzar.jj@gmail.com>
2021-11-16 11:22:56 +0630

    add Auth feature

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -15,6 +15,15 @@ use App\Http\Controllers\CategoriesController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 // admin dashboard routes
 Route::get('/admin', function () {
