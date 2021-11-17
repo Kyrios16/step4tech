e115dd54c2cbcf1057ea10c3e98437a551f407a4
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-11-17 14:20:00 +0630

    [feature/admin_analytics_dashboard]modified css and show most popular user

--- Laravel/resources/views/admin/analytic/analytics-manage.blade.php
+++ Laravel/resources/views/admin/analytic/analytics-manage.blade.php
@@ -20,8 +20,34 @@
             </header>
             <main>
                 @include('admin.common.analytics')
+                <div class="analytic-sec">
+                    @include('admin.common.chart')
+                    <div class="popular-user-card">
+                        <div class="text">
+                            <img src="https://www.shareicon.net/data/512x512/2016/09/15/829452_user_512x512.png" alt="">
+                            <h3>Jane Smith</h3>
+                            <p>Senior Developer</p>
+                            <div class="card-info">
+                                <h5 class="info-title">What is Laravel?</h5>
+                                <div class="analytic-card">
+                                    <div class="popular-icon">
+                                        <i class="fas fa-star"></i>
+                                    </div>
+                                    <div class="analytic-info">
+                                        <small>Post Rating</small>
+                                        <h2>4.9</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="links">
+                            <a target="_blank" href="https://codepen.io/l-e-e/"><i class="fab fa-codepen"></i></a>
+                            <a target="_blank" href="https://github.com/Leena26"><i class="fab fa-github"></i></a>
+                            <a href="https://facebook.com/pixelsum"><span class="fab fa-facebook-square"></span></a>
+                        </div>
+                    </div>
+                </div>
 
-                @include('admin.common.chart')
             </main>
         </div>
         <!-- main-content -->
