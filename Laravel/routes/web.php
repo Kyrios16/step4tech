712c8bd7e1c337d9669d95a5b6ffcc33730f67cd
maythinzar98 <maythinzar.jj@gmail.com>
2021-11-18 16:23:51 +0630

    [feature/user-function] add user function

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Post\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Feedback\FeedbackController;
 use App\Http\Controllers\Categories\CategoriesController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
