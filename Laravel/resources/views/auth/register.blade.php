83697dbad910658a30a56c27d29052e1ea209182
maythinzar98 <maythinzar.jj@gmail.com>
2021-11-18 15:44:40 +0630

    [feature/user-function] add user function

--- Laravel/resources/views/auth/register.blade.php
+++ Laravel/resources/views/auth/register.blade.php
@@ -20,21 +20,11 @@
 
 <body>
 
-
-  @if ($errors->any())
-  <div>
-    <ul>
-      @foreach($errors->all() as $error)
-      <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
-  @endif
-
   <div class="container">
     <div class="create-form-wrapper">
       <form method="post" action="{{ route('submit-register') }}" class="user-form" enctype="multipart/form-data">
         @csrf
+
         <div class="cover-img">
           <input type="file" class="file-upload" name="cover_img" onchange="preview_cover(event)">
           <img id="cover_preview" class="cover" alt="">
@@ -54,6 +44,11 @@
           <div class="formdata-control">
             <label for="email">Email Address <span>*</span></label><br>
             <input type="email" class="form-input" name="email" placeholder="Enter Email">
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
           </div>
 
           <div class="formdata-control">
@@ -69,6 +64,11 @@
           <div class="formdata-control">
             <label for="password_confirmatoion">Password Confirmation<span>*</span></label><br>
             <input type="password" class="form-input" name="password_confirmation" placeholder="Enter Password Again">
+            @error('password_confirmation')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
           </div>
 
           <div class="formdata-control">
@@ -102,7 +102,6 @@
           </div>
 
           <div class="formdata-control">
-
             <p class="text">Already have an account? <a href="{{ route('login')}}" class="link-text">Sign In</a>
             <div class="btn-group clearfix">
               <input type="submit" class="btn btn-success" name="create" value="Create">
