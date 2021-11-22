593e5dc47bae9df3bb5b621d61c9beb1b5f11e77
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-11-22 17:36:54 +0630

    [feature/admin_analytics_dashboard] fixed feature reviews

--- Laravel/app/Exports/UsersExport.php
+++ Laravel/app/Exports/UsersExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection, WithHeadings
 {
     /**
      * @return users list
@@ -13,5 +14,18 @@ class UsersExport implements FromCollection
     public function collection()
     {
         return User::all();
+    }
+
+    /**
+     * @return users table column 
+     */
+    public function headings(): array
+    {
+        return [
+            "Id", "Name", "Email", "Profile", "Cover",
+            "GitHub", "LinkedIn", "Bio", "DOB", "Phone", "Position", "Role",
+            "Created User Id", "Updated User Id", "Deleted User Id",
+            "Created_at", "Updated_at"
+        ];
     }
 }
