6e52bc89d9626bf7f2d8e7e6bd6be7ebb3f6a7d9
Zaw Win Tin <zawwintin21@gmail.com>
2021-12-02 15:14:27 +0630

    [feature/post_list] Add comments in loading.js

--- Laravel/public/js/common/loading.js
+++ Laravel/public/js/common/loading.js
@@ -1,8 +1,17 @@
+/**
+ * Show Loading Screen when Ajax Start
+ *
+ * @return void
+ */
 $(document).ajaxStart(function () {
     $(".loading-panel").show();
 });
 
+/**
+ * Hide Loading Screen after Ajax Stop
+ *
+ * @return void
+ */
 $(document).ajaxStop(function () {
     $(".loading-panel").animate({ opacity: 0.0 }, 500, function () { $(".loading-panel").css("visibility", "hidden"); });
-    // $(".loading-panel").css("display","none");
 });
