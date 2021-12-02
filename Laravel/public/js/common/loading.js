431893cd6e18f27470ce95301018ac15f502f908
Zaw Win Tin <zawwintin21@gmail.com>
2021-12-02 15:23:43 +0630

    [feature/post_list] Add Auto hide loading function

--- Laravel/public/js/common/loading.js
+++ Laravel/public/js/common/loading.js
@@ -13,5 +13,16 @@ $(document).ajaxStart(function () {
  * @return void
  */
 $(document).ajaxStop(function () {
-    $(".loading-panel").animate({ opacity: 0.0 }, 500, function () { $(".loading-panel").css("visibility", "hidden"); });
+    $(".loading-panel").animate({ opacity: 0.0 }, 500, function () { $(".loading-panel").hide(); });
+});
+
+/**
+ * Auto Hide Loading Screen
+ *
+ * @return void
+ */
+$(function() {
+    setTimeout(function() {
+        $(".loading-panel").animate({ opacity: 0.0 }, 500, function () { $(".loading-panel").hide(); });
+    }, 600);
 });
